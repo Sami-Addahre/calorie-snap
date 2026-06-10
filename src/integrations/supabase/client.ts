eb80df31e7f846c3dcd37eb15e3a662098db80c3
Sami-Addahre <sami.addahre@marconirovereto.it>
2026-06-10 13:21:37 +0000

    fix: add real supabase credentials

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,3 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient("https://placeholder.supabase.co", "placeholder");
+export const supabase = createClient(
+  "https://nhguhsewzppjstclbdcz.supabase.co/rest/v1/",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5oZ3Voc2V3enBwanN0Y2xiZGN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwODU5NjAsImV4cCI6MjA5NjY2MTk2MH0.dg68fztFgHfAjSrvysDRVTy-2Oh7FW3hG7JAPrn9BpU"
+);
